ef8584811b17150c6366d9611d48bd08baf14dcc
Natalia <nlouzau@gmail.com>
2019-09-23 17:44:52 -0300

    Primeros cambios al footer

--- home.php
+++ home.php
@@ -139,6 +139,7 @@
 
  <!-- FOOTER -->
  <footer class="container">
+   <p> HOLA PRUEBA DONDE APARECD ESTO </p>
    <p class="float-right"><a href="#">Back to top</a></p>
    <p>&copy; 2017-2019 Company, Inc. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
  </footer>

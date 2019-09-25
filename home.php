b7d181f80f120cfb34ece4be540ce42b05350e2d
rodolfodietl <rodolfo.dietl@me.com>
2019-09-25 16:34:10 -0300

    Fotos en el carrousel

--- home.php
+++ home.php
@@ -15,9 +15,7 @@
  <!--saque la nav a la navbar.php -->
  <?php include("navbar.php")?>
 </header>
-
-<main role="main">
-
+<div class="container">
  <div id="myCarousel" class="carousel slide" data-ride="carousel">
    <ol class="carousel-indicators">
      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
@@ -25,56 +23,50 @@
      <li data-target="#myCarousel" data-slide-to="2"></li>
    </ol>
    <div class="carousel-inner">
-     <div class="carousel-item active">
-       <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img"><rect width="100%" height="100%" fill="#777"/></svg>
-       <div class="container">
-         <div class="carousel-caption text-left">
-           <h1>Los mejores uniformes escolares</h1>
-           <p>Visite nuestra tienda de artículos</p>
-           <p><a class="btn btn-lg btn-primary" href="#" role="button">Visitar Galería</a></p>
-         </div>
-       </div>
-     </div>
-     <div class="carousel-item">
-       <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img"><rect width="100%" height="100%" fill="#777"/></svg>
-       <div class="container">
-         <div class="carousel-caption">
-           <h1>Todos los modelos</h1>
-           <p>Tenemos los uniformes para todos los colegios de Buenos Aires</p>
-           <p><a class="btn btn-lg btn-primary" href="#" role="button">Ver Listado</a></p>
-         </div>
-       </div>
-     </div>
-     <div class="carousel-item">
-       <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img"><rect width="100%" height="100%" fill="#777"/></svg>
-       <div class="container">
-         <div class="carousel-caption text-right">
-           <h1>Promociones!</h1>
-           <p>Visite nuestra sección de promociones!</p>
-           <p><a class="btn btn-lg btn-primary" href="#" role="button">Visitar</a></p>
-         </div>
+   <div class="carousel-item active">
+    <img src="img/carousel-1.jpg" alt="Carousel1">
+    <div class="container">
+      <div class="carousel-caption izquierda">
+        <h1>Los mejores uniformes escolares</h1>
+        <p>Visite nuestra tienda de artículos</p>
+        <p><a class="btn btn-lg btn-primary" href="#" role="button">Visitar Galería</a></p>
+      </div>
+    </div>
+  </div>
+    <div class="carousel-item">
+      <img src="img/carousel-2.jpg" alt="Carousel2">
+      <div class="container">
+        <div class="carousel-caption izquierda">
+          <h1>Todos los modelos</h1>
+          <p>Tenemos los uniformes para todos los colegios de Buenos Aires</p>
+          <p><a class="btn btn-lg btn-primary" href="#" role="button">Ver Listado</a></p>
+        </div>
+      </div>
+    </div>
+      <div class="carousel-item">
+       <img src="img/carousel-3.jpg" alt="Carousel3">
+       <div class="carousel-caption text-right izquierda">
+         <h1>Promociones!</h1>
+         <p>Visite nuestra sección de promociones!</p>
+         <p><a class="btn btn-lg btn-primary" href="#" role="button">Visitar</a></p>
        </div>
      </div>
    </div>
-   <a class="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev">
-     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-     <span class="sr-only">Previous</span>
-   </a>
-   <a class="carousel-control-next" href="#myCarousel" role="button" data-slide="next">
-     <span class="carousel-control-next-icon" aria-hidden="true"></span>
-     <span class="sr-only">Next</span>
-   </a>
- </div>
-
-
+         <a class="carousel-control-prev color" href="#myCarousel" role="button" data-slide="prev">
+       <span class="carousel-control-prev-icon color" aria-hidden="true"></span>
+       <span class="sr-only color ">Anterior</span>
+     </a>
+     <a class="carousel-control-next color" href="#myCarousel" role="button" data-slide="next">
+       <span class="carousel-control-next-icon color" aria-hidden="true"></span>
+       <span class="sr-only color">Próxima</span>
+     </a>
+       </div>
+       </div>
  <!-- Marketing messaging and featurettes
  ================================================== -->
  <!-- Wrap the rest of the page in another container to center all the content. -->
-
  <div class="container marketing">
-
    <!-- START THE FEATURETTES -->
-
    <div class="row featurette">
      <div class="col-md-7">
        <h2 class="featurette-heading">First featurette heading. <span class="text-muted">It’ll blow your mind.</span></h2>
@@ -84,9 +76,7 @@
        <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 500x500"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"/><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text></svg>
      </div>
    </div>
-
    <hr class="featurette-divider">
-
    <div class="row featurette">
      <div class="col-md-7 order-md-2">
        <h2 class="featurette-heading">Oh yeah, it’s that good. <span class="text-muted">See for yourself.</span></h2>
@@ -96,9 +86,7 @@
        <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 500x500"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"/><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text></svg>
      </div>
    </div>
-
    <hr class="featurette-divider">
-
    <div class="row featurette">
      <div class="col-md-7">
        <h2 class="featurette-heading">And lastly, this one. <span class="text-muted">Checkmate.</span></h2>
@@ -108,28 +96,15 @@
        <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 500x500"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"/><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text></svg>
      </div>
    </div>
-
    <hr class="featurette-divider">
-
    <!-- /END THE FEATURETTES -->
-
  </div><!-- /.container -->
-
-
  <!-- FOOTER -->
  <footer class="container">
    <p class="float-right"><a href="#">Back to top</a></p>
    <p>&copy; 2017-2019 Company, Inc. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
  </footer>
 </main>
-
-
-
-
-
-
-
-
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>

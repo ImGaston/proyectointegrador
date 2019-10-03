32bf6bcf0e3ccffdd9e32a06c34edecfaa71c66f
rodolfodietl <rodolfo.dietl@me.com>
2019-10-03 15:18:42 -0300

    Navbar responsive

--- navbar.php
+++ navbar.php
@@ -1,36 +1,62 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-   <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="#">
-        <img src="img/logo.jpg" width="30" height="30" class="d-inline-block align-top" alt="">
-        Pitagoras'c
-    </a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target= "#navbarCollapse" aria-controls="navbarCollapse"aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbar Collapse">
-        <ul class="navbar-nav mr-auto">
-              <li class="nav-item active">
-                <a class="nav-link" href="home.php">Home<span class="sr-only">(current)</span></a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="productos.php">Productos</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="faq.php">Preguntas Frecuentes</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="contacto.php">Contacto</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="registro.php">Registro</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="login.php">Login</a>
-              </li>
-        </ul>
-          <form class="form-inline mt-2 mb-0 mt-md-0">
-            <input class="form-control mr-sm-2" type="text" placeholder="Buscar" aria-label="BUSCAR">
-            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">BUSCAR</button>
-          </form>
-          <a class="perfil" href="perfil.php"><ion-icon size="large" name="contact"></ion-icon></a>
-      </div>
+    <link href="https://fonts.googleapis.com/css?family=Lato|Montserrat:400,700&display=swap" rel="stylesheet">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <title></title>
+  </head>
+  <body>
+    <div class="container">
+      <div class="row">
+        <div class="col-12 py-2">
+
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+     <a class="navbar-brand" href="#">
+         <img src="img/logo.jpg" width="30" height="30" class="d-inline-block align-top" alt="">
+         Pitagoras'c
+     </a>
+
+     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+       <span class="navbar-toggler-icon"></span>
+     </button>
+
+     <div class="collapse navbar-collapse" id="navbarSupportedContent">
+         <ul class="navbar-nav mr-auto">
+               <li class="nav-item active">
+                 <a class="nav-link" href="home.php">Home<span class="sr-only">(current)</span></a>
+               </li>
+               <li class="nav-item">
+                 <a class="nav-link" href="productos.php">Productos</a>
+               </li>
+               <li class="nav-item">
+                 <a class="nav-link" href="faq.php">Preguntas Frecuentes</a>
+               </li>
+               <li class="nav-item">
+                 <a class="nav-link" href="contacto.php">Contacto</a>
+               </li>
+               <li class="nav-item">
+                 <a class="nav-link" href="registro.php">Registro</a>
+               </li>
+               <li class="nav-item">
+                 <a class="nav-link" href="login.php">Login</a>
+               </li>
+         </ul>
+           <form class="form-inline mt-2 mb-0 mt-md-0">
+             <input class="form-control mr-sm-2" type="text" placeholder="Buscar" aria-label="BUSCAR">
+             <button class="btn btn-outline-success my-2 my-sm-0" type="submit">BUSCAR</button>
+           </form>
+           <a class="perfil" href="perfil.php"><ion-icon size="large" name="contact"></ion-icon></a>
+           </div>
+   </div>
+ </div>
+  </div>
+
+  </body>
+</html>

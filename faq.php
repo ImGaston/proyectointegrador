ede8f9554386e21c9c6fae72d4d374687e3c59ac
rodolfodietl <rodolfo.dietl@me.com>
2019-10-14 19:39:00 -0300

    Olvide mi contraseña y FAQ responsive

--- faq.php
+++ faq.php
@@ -8,6 +8,9 @@
 <!DOCTYPE html>
 <html lang="es">
   <head>
+	<!-- Bootstrap CSS -->
+		<link href="https://fonts.googleapis.com/css?family=Merriweather&display=swap" rel="stylesheet">
+
     <meta charset="utf-8">
     <title>faq</title>
       <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
@@ -18,12 +21,16 @@
 <body>
          <?php include("navbar.php")?>
 <!--esto corresponde a la FAQ-->
-</div>
-
-<nav>
-    <div class="container text-center" >
+  <div class="container text-center" >
         <br>
-        <article class="Imagen">
+        <article class="">
+					  <br>
+						<div class="container text-center">
+							<div class="row text-center" id="imagen">
+								<div class="col-12 col-md-6 col-lg-4 text-center" id="img">
+									<img src="https://previews.123rf.com/images/coramax/coramax1208/coramax120801252/14800724-gente-3d-hombre-persona-y-preguntas-m%C3%A1s-frecuentes-faq-concepto.jpg" class="img-fluid" alt="Resultado de imagen para imagenes para faq" width="400" height="350">
+								  </div>
+                  </div>
         </article>
         <br>
         <details>
@@ -56,7 +63,8 @@
               <p>Trabajamos con colegios de zona norte.</p>
       </details>
    </div>
-</nav>
+</div>
+
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
--- login.php
+++ login.php
@@ -38,7 +38,9 @@
 
 	$pageTitle = 'Login';
 
-	require_once 'navbar.php'; ?>
+	require_once 'navbar.php';
+
+	?>
 
 
 <html lang="es">
@@ -97,9 +99,43 @@
         <p>¿Todavía no sos cliente?<a href="registro.php"> ¡Crea una Cuenta!</a></p>
 
       </div>
+			</div>
+			<br>
+			<div class="row">
+				<div class="col">
+					<a href="#myModal1" data-toggle="modal" data-target="#myModal1">Olvidé mi Contraseña</a>
 
-    </div>
+				</div>
+				</div>
   </div>
+
+	<div class="modal fade" id="myModal1" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+		<div class="modal-dialog" role="document">
+			<div class="modal-content">
+				<div class="modal-header">
+					<h5 class="modal-title" id="exampleModalLabel">Qué tipo olvidadizo...</h5>
+					<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+						<span aria-hidden="true">&times;</span>
+					</button>
+				</div>
+				<div class="modal-body">
+				Ingresá tu correo electrónico y te la enviamos.
+			</div>
+							<div class="modal-body">
+					<label for="consulta1"></label>
+					<div class="form-label-group">
+						<input type="Email" id="inputEmail" class="form-control" name = "email1" method="post" placeholder="Email" required>
+					</div>
+					</div>
+				<div class="modal-footer">
+					<button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+					<a href="olvido.php">
+					<button type="button" class="btn btn-primary">Enviar</button>
+					</a>
+						</div>
+			</div>
+		</div>
+	</div>
   <!-- FOOTER -->
  <footer class="container">
    <?php include_once 'footer.php'; ?>

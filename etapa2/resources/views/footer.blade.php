c3db645982b079a955480ec280e60128736d0748
Gaston <gaston@pitagorasc.com.ar>
2019-11-16 23:14:38 -0300

    layout.blade.php

---
+++ etapa2/resources/views/footer.blade.php
@@ -0,0 +1,65 @@
+<footer class="footer_part">
+            <div class="container mt-4">
+                <div class="row justify-content-between">
+                    <div class="col-sm-6 col-lg-2">
+                        <div class="single_footer_part">
+                            <h4>Categorías</h4>
+                            <ul class="list-unstyled">
+                                <li><a class="text-dark" href="http://<?php echo $_SERVER['HTTP_HOST']?>/proyectointegrador/productos.php#colegial">Colegios</a></li>
+                                <li><a class="text-dark" href="http://<?php echo $_SERVER['HTTP_HOST']?>/proyectointegrador/productos.php#egresados">Egresados</a></li>
+                                <li><a class="text-dark" href="http://<?php echo $_SERVER['HTTP_HOST']?>/proyectointegrador/productos.php#servicios">Hospitalario</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-sm-6 col-lg-2">
+                        <div class="single_footer_part">
+                            <h4>Nosotros</h4>
+                            <ul class="list-unstyled">
+                                <li><a class="text-dark" href="http://<?php echo $_SERVER['HTTP_HOST']?>/proyectointegrador/nosotros.php">Nosotros</a></li>
+                                <li><a class="text-dark" href="http://<?php echo $_SERVER['HTTP_HOST']?>/proyectointegrador/faq.php">FAQ</a></li>
+                                <li><a href="#myModal" data-toggle="modal" data-target="#myModal" class="text-dark">Contacto</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-sm-6 col-lg-3">
+                        <div class="single_footer_part">
+                            <h4>Dirección</h4>
+                            <ul class="list-unstyled">
+                                <li><a href="#"class="text-dark">Padre Patiño 847, Formosa</a></li>
+                                <li><a class="text-dark" href="#">(370) 4627924</a></li>
+                                <li><span>info@pitagorasc.com.ar</span></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-sm-6 col-lg-4">
+                        <div class="single_footer_part">
+                            <h4>Newsletter</h4>
+                            <div id="mc_embed_signup">
+                                <form target="home.php"
+                                    action="newsletter.php"
+                                    method="get" class="subscribe_form relative mail_part">
+                                    <input type="email" name="email" id="newsletter-form-email" placeholder="Email"
+                                        class="placeholder hide-on-focus" onfocus="this.placeholder = ''"
+                                        onblur="this.placeholder = ' Email Address '">
+                                    <button type="submit" class="btn btn-light" name="submit" id="newsletter-submit"
+                                        class="email_icon newsletter-submit button-contactForm">Suscribite</button>
+                                    <div class="mt-10 info"></div>
+                                </form>
+                            </div>
+                            <div class="social_icon">
+                                <a href="#"><i class="ti-facebook"></i></a>
+                                <a href="#"><i class="ti-twitter-alt"></i></a>
+                                <a href="#"><i class="ti-instagram"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row justify-content-center">
+                    <div class="col-lg-12">
+                        <div class="copyright_text">
+                            <P>Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | Pitagoras'c</P>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
---
+++ etapa2/resources/views/header.blade.php
@@ -0,0 +1,84 @@
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container">
+            <a class="navbar-brand" href="http://<?php echo $_SERVER['HTTP_HOST']?>/proyectointegrador/home.php">
+            <img src="http://<?php echo $_SERVER['HTTP_HOST']?>/proyectointegrador/img/logo.jpg" width="30" height="30" class="d-inline-block align-top" alt="">
+            Pitagoras'c
+            </a>
+
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarCollapse">
+                <ul class="navbar-nav mr-auto justify-content-end">
+                    <li class="nav-item">
+                    <a class="nav-link" href="http://<?php echo $_SERVER['HTTP_HOST']?>/proyectointegrador/nosotros.php">Nosotros</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Tienda
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="http://<?php echo $_SERVER['HTTP_HOST']?>/proyectointegrador/productos.php">Ver todo</a>
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="http://<?php echo $_SERVER['HTTP_HOST']?>/proyectointegrador/productos.php#colegial">Colegios</a>
+                        <a class="dropdown-item" href="http://<?php echo $_SERVER['HTTP_HOST']?>/proyectointegrador/productos.php#egresados">Egresados</a>
+                        <a class="dropdown-item" href="http://<?php echo $_SERVER['HTTP_HOST']?>/proyectointegrador/productos.php#servicios">Hospitalario</a>
+                    </div>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link" href="http://<?php echo $_SERVER['HTTP_HOST']?>/proyectointegrador/mayorista.php">Mayorista</a>
+                    </li>
+                    <li class="nav-item">
+                    <a href="#myModal" data-toggle="modal" data-target="#myModal" class="nav-link">Contacto</a>
+                    </li>
+                </ul>
+
+                <ul class="navbar-nav ml-auto" style="display: flex">
+                <!-- TODA LA PARTE DE LOGIN ESTA COMENTADA PARA NO GENERAR ERROR -->
+                <!-- Preguntamos si NO está logueado el usuario para mostrar los enlaces Register y Login -->
+               
+                    <li class="nav-item"><a class="nav-link" href="http://<?php echo $_SERVER['HTTP_HOST']?>/proyectointegrador/registro.php">Registrate</a></li>
+                    <li class="nav-item"><a class="nav-link" href="http://<?php echo $_SERVER['HTTP_HOST']?>/proyectointegrador/login.php">Login</a></li>
+                
+                </ul>
+            </div>
+        </div>
+</nav>
+    <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Contacto</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <div class="row">
+                <div class="col">
+                    <input type="text" class="form-control" placeholder="Nombre">
+                </div>
+                <div class="col">
+                    <input type="text" class="form-control" placeholder="Apellido">
+                </div>
+                </div>
+                <label for="consulta1"></label>
+                <div class="form-label-group">
+                <input type="Email" id="inputEmail" class="form-control" placeholder="Email" required>
+                </div>
+
+                <div class="form-group">
+                <label for="consulta1"></label>
+                <textarea class="form-control" id="consulta1" rows="4" placeholder="Dejanos tu consulta aca"></textarea>
+                </div>
+
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+                <a href="http://<?php echo $_SERVER['HTTP_HOST']?>/proyectointegrador/gracias.php"> <button type="button" class="btn btn-primary">Enviar</button> </a>
+            </div>
+            </div>
+        </div>
+    </div>

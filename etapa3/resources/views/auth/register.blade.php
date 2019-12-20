59ecc3c23cc6910183e3cad9176f6b30f74b9e48
rodolfodietl <rodolfo.dietl@me.com>
2019-12-20 09:13:28 -0300

    arreglos en formularios

--- etapa3/resources/views/auth/register.blade.php
+++ etapa3/resources/views/auth/register.blade.php
@@ -1,5 +1,10 @@
 @extends('layout')
 
+<script
+  src="https://code.jquery.com/jquery-3.3.1.min.js"
+  integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+  crossorigin="anonymous"></script>
+
 @section('title')
 Pitagoras | Registrate
 @endsection
@@ -68,6 +73,7 @@ Pitagoras | Registrate
                             <div class="form-group row">
                                 <label for="avatar" class="col-md-4 col-form-label text-md-right">Avatar</label>
 
+  <div class="col-md-6">
                         <div class="custom-file">
                           <input type="file" name="avatar" class="custom-file-input form-control"
                           >
@@ -82,7 +88,7 @@ Pitagoras | Registrate
                           </script>
                         </div>
                         </div>
-
+  </div>
 
 
                               <div class="form-group row mb-0">
@@ -98,4 +104,7 @@ Pitagoras | Registrate
         </div>
     </div>
 </div>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 @endsection
